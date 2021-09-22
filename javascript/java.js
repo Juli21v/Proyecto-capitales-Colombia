d28148c880015bc5ab8dcb9bf67a766c1a139b5e
Juli21v <julianvar212@gmail.com>
2021-09-21 19:12:34 -0500

    pimer commit

---
+++ javascript/java.js
@@ -0,0 +1,104 @@
+const nombdepart = () => {
+  let departamento = prompt("Ingresa un departamento de Colombia");
+  switch (departamento) {
+    case "amazonas":
+      alert("Leticia");
+      break;
+    case "antioquia":
+      alert("Medellín");
+      break;
+    case "arauca":
+      alert("Arauca");
+      break;
+    case "atlantico":
+      alert("Barranquilla");
+      break;
+    case "bolivar":
+      alert("Cartagena de indias");
+      break;
+    case "boyaca":
+      alert("Tunja");
+      break;
+    case "caldas":
+      alert("Manizales");
+      break;
+    case "caqueta":
+      alert("Florencia");
+      break;
+    case "casanare":
+      alert("Yopal");
+      break;
+    case "cauca":
+      alert("Popayán");
+      break;
+    case "cesar":
+      alert("Valledupar");
+      break;
+    case "chocó":
+      alert("Quibdó");
+      break;
+    case "cordoba":
+      alert("Montería");
+      break;
+    case "cundinamarca":
+      alert("Bogotá");
+      break;
+    case "guainia":
+      alert("Inrida");
+      break;
+    case "guaviare":
+      alert("San José del Guaviare");
+      break;
+    case "huila":
+      alert("Neiva");
+      break;
+    case "la guajira":
+      alert("Riohacha");
+      break;
+    case "magdalena":
+      alert("Santa Marta");
+      break;
+    case "meta":
+      alert("Villavicencio");
+      break;
+    case "nariño":
+      alert("San Juan de Pasto");
+      break;
+    case "norte de santander":
+      alert("San José de Cúcuta");
+      break;
+    case "putumayo":
+      alert("Mocoa");
+      break;
+    case "quindio":
+      alert("Armenia");
+      break;
+    case "risaralda":
+      alert("Pereira");
+      break;
+    case "san andres y providencia":
+      alert("San Andrés");
+      break;
+    case "santander":
+      alert("Bucaramanga");
+      break;
+    case "sucre":
+      alert("Sincelejo");
+      break;
+    case "tolima":
+      alert("Ibagué");
+      break;
+    case "valle del cauca":
+      alert("Cali");
+      break;
+    case "vaupés":
+      alert("Mitú");
+      break;
+    case "vichada":
+      alert("Puerto Carreño");
+      break;
+    default:
+      alert("Eso no es un departamento de Colombia");
+      break;
+  }
+};
